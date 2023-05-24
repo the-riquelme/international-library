461971fce9bc8a5ca46182f727ac69af6d1b18c7
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-05-24 02:56:15 -0300

    :sparkles: feat: implement CambioRepository

---
+++ cambio-service/src/main/java/br/com/riquelme/cambioservice/repository/CambioRepository.java
@@ -0,0 +1,11 @@
+package br.com.riquelme.cambioservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.riquelme.cambioservice.model.Cambio;
+
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+	Cambio findByFromAndTo(String from, String to);
+
+}
