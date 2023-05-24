c74881028f8070d1d8d5d4886a8baa6856bfc635
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-05-24 02:19:21 -0300

    :bricks: ci: add cambio-db in docker-compose file

--- cambio-service/src/main/java/br/com/riquelme/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/riquelme/cambioservice/controller/CambioController.java
@@ -2,6 +2,8 @@ package br.com.riquelme.cambioservice.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,14 @@ import br.com.riquelme.cambioservice.model.Cambio;
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
+
+	@Autowired
+	private Environment environment;
 	
 	@GetMapping(value = "/{amount}/{from}/{to}")
 	public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to) {
 
-		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+    var port = environment.getProperty("local.server.port");
+		return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 }
