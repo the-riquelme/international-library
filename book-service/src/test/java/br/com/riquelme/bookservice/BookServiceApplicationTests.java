8ac5dd2b77e69a77e4602ba136e6f05dd74167ad
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-05-24 04:43:23 -0300

    :bricks: ci: adds basic structure of book service

---
+++ book-service/src/test/java/br/com/riquelme/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.riquelme.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
