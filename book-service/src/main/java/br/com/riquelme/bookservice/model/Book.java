b06b34c7a3ad848dc51c16cdbde740a181765979
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-05-28 14:11:24 -0300

    :sparkles: feat: Map Book model to your DB table

--- book-service/src/main/java/br/com/riquelme/bookservice/model/Book.java
+++ book-service/src/main/java/br/com/riquelme/bookservice/model/Book.java
@@ -3,22 +3,39 @@ package br.com.riquelme.bookservice.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
 public class Book implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(name = "author", nullable = false, length = 180)
 	private String author;
 	
+	@Column(name = "launch_date", nullable = false)
+	@Temporal(TemporalType.DATE)
 	private Date launchDate;
-
+	
+	@Column(nullable = false)
 	private Double price;
-
+	
+	@Column(nullable = false, length = 250)
 	private String title;
-
+	
+	@Transient
 	private String currency;
-
+	
+	@Transient
 	private String environment;
 	
 	public Book() {}
