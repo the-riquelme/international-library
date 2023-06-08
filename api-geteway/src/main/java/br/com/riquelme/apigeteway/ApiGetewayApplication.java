a3cbce9246724cfbfa2d2572ac4da1ce47a926c8
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-06-08 10:53:01 -0300

    :bricks: ci: adds basic structure of api gateway

---
+++ api-geteway/src/main/java/br/com/riquelme/apigeteway/ApiGetewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.riquelme.apigeteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetewayApplication.class, args);
+	}
+
+}
---
+++ api-geteway/src/test/java/br/com/riquelme/apigeteway/ApiGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.riquelme.apigeteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
